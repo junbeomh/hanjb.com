52074a15fba4786b3b848c49587533fc3c320745
JunBeom Han <junbeomh94@gmail.com>
2020-09-26 17:06:04 -0700

    linked navbar to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <p> React Boilerplate</p>
-    </div>
+    <Navbar/>
+  </div>
   );
 }
 
