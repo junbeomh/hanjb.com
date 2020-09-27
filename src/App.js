740c49d06f634c6fbe87dcad16c4d2d36c71e457
junbeomh <junbeomh94@gmail.com>
2020-09-26 19:52:13 -0700

    connected Navbar and Sections to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./Components/Navbar";
+import About from './Sections/About';
+import Projects from './Sections/Projects';
+import Experience from './Sections/Experience';
+import Skills from './Sections/Skills';
+import Education from './Sections/Education';
+import Contact from './Sections/Contact';
+
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
+      <Navbar></Navbar>
+      <About></About>
+      <Projects></Projects>
+      <Experience></Experience>
+      <Skills></Skills>
+      <Education></Education>
+      <Contact></Contact>
   </div>
   );
 }
