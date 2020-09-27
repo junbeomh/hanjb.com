496558c2d29f79e180f4824a69f56cf7582752ee
JunBeom Han <junbeomh94@gmail.com>
2020-09-27 00:22:21 -0700

    added main page

---
+++ src/Sections/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <div className="main-section" id="main"> </div>
+        );
+    }
+}
+
+export default Main;
