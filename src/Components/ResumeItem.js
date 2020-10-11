42085b22507a4c0f609e6406f0e178fa9ef7d726
junbeomh <junbeomh94@gmail.com>
2020-10-11 15:54:48 -0700

    updated styles

--- src/Components/ResumeItem.js
+++ src/Components/ResumeItem.js
@@ -3,49 +3,42 @@ import styled from 'styled-components';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const ResumeItemDiv = styled.div`
-    border-radius: 10px;
-    // background-color: rgba(255, 255, 255, 0.7);
-    // box-shadow: inset 0 2px 2px rgba(0,0,0,0.16), 0 2px 2px rgba(0,0,0,0.45);
     padding: 2rem;
-    margin-bottom: 2rem;
     max-width: 50rem;
     @media screen and (max-width: 991px) {
             width: 100%;
-            border-radius: 10px;
             padding: 1.1rem;
     }
+
     @media screen and (max-width: 480px) {
             width: 20rem;
-            border-radius: 10px;
             padding: 1rem;
+    }
+    
+    @media screen and (max-width: 768px){
+        .resume-item-header-date {
+            size: 10px;
+        }
+    }
+    @media screen and (max-width: 480px){
+        .resume-item-header-date {
+            size: 10px;
+        }
     }
 `;
 
-const ResumeHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    @media screen and (max-width: 480px) {
-        .resume-item-header-company {
-            max-width: 10rem;
-            padding-right: 0.2rem;
-        }
-    }
-    .resume-item-header-date {
-        display: grid	;
-    }
-`;
 
 const ResumeSubHeader = styled.div`
     padding-top: 0.5rem;
     padding-bottom: 0.5rem;
-    h5{
+    @media screen and (max-width: 768px){
+        padding: 0rem;
+    }
+    @media screen and (max-width: 480px){
+        padding: 0rem;
     }
 `;
 
-const ResumeDetails = styled.ul`
-    list-style: square;
-    padding-left: 3%;
-`;
 
 const StyledText = styled.div`
   ul.content-list {
@@ -67,40 +60,26 @@ const StyledText = styled.div`
   }
 `;
 
-const AboutContainer = styled.div`
-
-}
-
-`;
 
 function ResumeItem(props) {
-    const lines = [
-        " Waited end mutual missed myself the little sister one. So in pointed or chicken cheered neither spirits invited.",
-        " Waited end mutual missed myself the little sister one. So in pointed or chicken cheered neither spirits invited.",
-        " Waited end mutual missed myself the little sister one. So in pointed or chicken cheered neither spirits invited.",
-    ]
     return (
         <ResumeItemDiv>
             <Container>
-                <AboutContainer>
                     <Row>
                         <Col sm={8}>
-                            <h3 className="resume-item-header-company"> WORD OF NEW LIFE BOOKS </h3>
+                            <h3 className="resume-item-header-company text-emphasis"> {props.place} </h3>
 
                         </Col>
                         <Col sm={4}>
-                            <h6 > <span className="resume-item-header-date"> DEC. 2020 - </span> <span className="resume-item-header-date">  DEC. 2020 </span> </h6>
-
+                            <p  className="resume-item-header-date"> <span className="resume-item-header-date"> DEC. 2020 - </span> <span className="resume-item-header-date">  DEC. 2020 </span> </p>
                         </Col>
                     </Row>
-                </AboutContainer>
 
-                <ResumeSubHeader> <h5> Software Developer Co-op </h5> </ResumeSubHeader>
+                <ResumeSubHeader> <h5> {props.major} </h5> </ResumeSubHeader>
 
                 <StyledText>
-
                     <ul className="content-list">
-                        {lines && lines.map((line, i) => <li key={i}> <span> {line} </span></li>)}
+                        {props.desription  && props.desription .map((line, i) => <li key={i}> <span> {line} </span></li>)}
                     </ul>
 
                 </StyledText>
--- src/Sections/Education.js
+++ src/Sections/Education.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import ResumeItem from '../Components/ResumeItem';
 
-
-
 const ContentWrapper = styled.div`
     padding: 1.5rem;
     display: flex;
@@ -12,11 +10,12 @@ const ContentWrapper = styled.div`
     margin: 0 auto;
     .resume-content-title {
         margin-top: 3rem;
-        margin-bottom: 5rem;
+        margin-bottom: 3rem;
         display: flex;
         justify-content: center;
         align-items: center;
     }
+    // margin-bottom: 15rem;
 `;
 const PAGE_NAME = "04. EDUCATION."
 
@@ -26,12 +25,22 @@ const Education = () => {
         <section id="education">
             <ContentWrapper>
                 <div className="resume-content-title">
-                    <span className="header-number" style={{ color: "#DBA111", fontSize: "25px", verticalAlign: "bottom", verticalAlign: "top" }}> 04. &nbsp;&nbsp;</span>
+                    <span className="header-number" style={{ color: "#DBA111", fontSize: "25px", verticalAlign: "bottom", verticalAlign: "top" }}> 04. &nbsp;</span>
                     <span className="header" style={{ color: "rgba(255, 255, 255, 0.9)", fontSize: "33px", verticalAlign: "top" }}> EDUCATION.</span>
                 </div>
                 <div className="resume-items">
-                    <ResumeItem />
-                    <ResumeItem />
+                    <ResumeItem
+                        place={"British Columbia Institute of Technology"}
+                        major={"Computer Systems Technology Diploma"}
+                        date={"Sept 2018 - Dec 2020"}
+                        description={null}
+                    />
+                    <ResumeItem
+                        place={"University of British Columbia"}
+                        major={"Bachelor of Arts Degree with Sociology"}
+                        date={"Sept 2012 - May 2016"}
+                        description={null}
+                    />
                 </div>
             </ContentWrapper>
         </section>
