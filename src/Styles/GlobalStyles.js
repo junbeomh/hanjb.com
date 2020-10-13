840367cb4c0e7ea84a1e96ef1d1e0eef23b9f4ad
junbeomh <junbeomh94@gmail.com>
2020-10-13 15:57:06 -0700

    added yellow color variable and cleaned up

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -15,6 +15,7 @@ export const GlobalStyles = createGlobalStyle`
     --font-main: 'ROBOTO';
     --font-name: 'Sacramento';
     --color-emphasis: rgba(255, 255, 255, 0.6);
+    --color-yellow: #DBA111;
   }
 
   .big-heading {
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,38 +1,3 @@
-// const base = {
-//   easeOutBack: "cubic-bezier(0.34, 1.56, 0.64, 1)",
-//   colorWhite: "rgb(255, 255, 255)",
-//   colorBlack: "rgb(0, 0, 0)",
-
-// };
-
-// const dark = {
-//   id: "dark",
-//   ...base,
-//   primary: "#121212",
-//   backgroundColor: "linear-gradient(to bottom, #121212 0%, #000 100%)",
-//   textColor: "rgba(255, 255, 255, 0.6)",
-//   navColor: "black",
-//   buttonColor: "rgba(255, 255, 255, 0.6)",
-//   buttonBorder: "1px solid rgba(255, 255, 255, 0.6)",
-//   buttonHover: ' rgba(255, 255, 255, 0.1)',
-
-// };
-
-// const light = {
-//   id: "light",
-//   ...base,
-//   primary: "#7de2fc",
-//   backgroundColor: "linear-gradient(to bottom, #034694 0%, #034694 100%)",
-//   textColor: "rgba(255, 255, 255, 0.6)",
-//   navColor: "#023876",
-//   buttonColor: "rgba(255, 255, 255, 0.6)",
-//   buttonBorder: "1px solid rgba(255, 255, 255, 0.6)",
-//   buttonHover: ' rgba(255, 255, 255, 0.1)',
-// };
-
-// export const theme = { dark, light };
-
-// // backgroundColor: "linear-gradient(to bottom, #7de2fc 0%, rgba(230, 230, 230, 0.5) 100%)",
 const base = {
   easeOutBack: "cubic-bezier(0.34, 1.56, 0.64, 1)",
   colorWhite: "rgb(255, 255, 255)",
@@ -44,7 +9,7 @@ export const dark = {
   id: "dark",
   ...base,
   primary: "#121212",
-  backgroundColor: "linear-gradient(to bottom, #121212 0%, #000 100%)",
+  backgroundColor: "linear-gradient(to bottom, #121212 0%, #121212 100%)",
   textColor: "rgba(255, 255, 255, 0.6)",
   navColor: "black",
   buttonColor: "rgba(255, 255, 255, 0.6)",
