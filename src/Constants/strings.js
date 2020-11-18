b810bdf4c01230c7ab5a2cb543a8f71086530ca5
junbeomh <junbeomh94@gmail.com>
2020-11-17 23:08:36 -0800

    update

---
+++ src/Constants/strings.js
@@ -0,0 +1,104 @@
+
+
+module.exports = Object.freeze({
+    MAIN: {
+        NAME: "JunBeom Han.",
+        POSITION: "Junior Software Developer.",
+        ABOUT_SHORT: " I am a CS student based in Vancouver, BC, Canada. I enjoy building things and discover how they are made. Also, I love playing soccer, snowboarding, and surfing!",
+        GREETING: "Hi, my name is",
+    },
+    ABOUT: {
+        INDEX: "01.",
+        TITLE: "ABOUT ME.",
+        GREETING: "Hey! I'm JunBeom, a junior software engineer based in Vancouver, Canada.",
+        BACKGROUND: "Shortly after graduating from UBC, I have decided to make a career switch to Computer Science - and I am loving it since!",
+        ABOUT_LONG: " I am devoted to continuous learning, as there are always new perspectives unseen. Also, I love having structure and keeping things in order, with a mindset of continuously improving and optimizing them. Lastly, I enjoy and value teamwork because accomplishing collective goals as a group is always thrilling.",
+        TECH_STACK: "Here are a few technologies I've been working with recently:",
+        SKILLS: ['AWS', 'React', 'Python', 'Node & Express JS', 'MongoDB', 'Docker'],
+        RESUME: "RESUME"
+    },
+
+    PROJECTS: {
+        INDEX: "02.",
+        TITLE: "PROJECTS.",
+        // ITEMS: [
+        //     // {
+        //     //     appCompany: "LoveYourPlanet",
+        //     //     appName: "LoveYourPlanet Mobile",
+        //     //     appDescrip: "",
+        //     //     img: LoveYourPlanet
+        //     // },
+        //     {
+        //         appCompany: "Personal Project",
+        //         appName: "FootyRoom",
+        //         appDescrip: "",
+        //         img: FootyRoom
+        //     },
+        //     {
+        //         appCompany: "BCITSA",
+        //         appName: "BCITSAVotes",
+        //         appDescrip: "",
+        //         img: BCITSAVOTES
+        //     },
+        // ],
+    },
+    
+    EXPERIENCE: {
+        INDEX: "03.",
+        TITLE: "EXPERIENCE.",
+        EXPERIENCES: [
+            {
+                name: "Piccolo Software Inc.",
+                date: "May 2019 - Dec 2019",
+                position: "Software Dev. Co-op",
+                details: "Android, Node.js, VB.Net, Microsoft Web-services",
+
+            },
+            {
+                name: "Word of New Life Books",
+                date: "Jan 2016 - Jan 2017",
+                position: "Floor Manager",
+                details: "Customer support and relations, inventory management",
+            },
+            {
+                name: "JCrew",
+                date: "May 2014 - May 2015",
+                position: "Sales Associate",
+                details: "Customer service ",
+            },
+        ],
+    },
+
+    EDUCATION: {
+        INDEX: "04.",
+        TITLE: "EDUCATION.",
+        EDUCATIONS: [
+            {
+                name: "British Columbia Institute of Technology",
+                date: "Sept 2018 - Dec 2020",
+                major: "Computer Systems Technology Diploma",
+            },
+            {
+                name: "University of British Columbia",
+                date: "Sept 2012 - May 2016",
+                major: "Bachelor of Arts Degree with Sociology",
+            },
+        ],
+    },
+
+    SOCIAL_NETWORKS: {
+        EMAIL: "junbeomh94@gmail.com",
+        GITHUB: "https://github.com/junbeomh",
+        LINKEDIN: "https://www.linkedin.com/in/johnjbhan/",
+        INSTAGRAM: "https://www.instagram.com/hanjbbb/",
+    },
+
+    NAVBAR: {
+
+    },
+
+    FOOTER: {
+
+    }
+
+});
